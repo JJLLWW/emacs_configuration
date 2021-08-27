ae0ac62b3a31a53337d8226099def8a3cd9c3dfc
Jack Wright <jack.louie.wright@gmail.com>
2021-08-27 19:15:22 +0100

    autocompletion working

--- lisp/lsp.el
+++ lisp/lsp.el
@@ -2,13 +2,19 @@
 
 ;; needs a system clangd, which itself requires clang and libtool
 
+(straight-use-package 'cmake-mode)
+
 ;; Hide warning "LSP :: Yasnippet is not installed, but `lsp-enable-snippet' is set to `t'."
 (setq lsp-enable-snippet nil)
 
+;; disable the annoying top bar
+(setq lsp-headerline-breadcrumb-enable nil)
+
 ;; flymake causes errors when loading a saved desktop "error in process sentinel: flymake-error ..."
 (straight-use-package 'flycheck)
-
-(straight-use-package 'cmake-mode)
+;; autocompletion frontend
+(straight-use-package 'company)
+(straight-use-package 'company-lsp)
 (straight-use-package 'lsp-mode)
 
 ; we really want to bail out based on the time clangd takes to process the file
