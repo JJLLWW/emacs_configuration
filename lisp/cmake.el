4a610db4366ae5ce97970dc68ccc111038b83bd4
Jack Wright <jack.louie.wright@gmail.com>
2022-02-05 13:27:00 +0000

    cmake laanguage server

--- lisp/cmake.el
+++ lisp/cmake.el
@@ -1,4 +1,11 @@
 (straight-use-package 'cmake-mode)
 
 ; cmake autocompletion
-(add-hook 'cmake-mode-hook 'company-cmake)
+(add-hook 'cmake-mode-hook 'company-mode)
+;; (add-hook 'cmake-mode-hook 'company-cmake)
+
+; cmake language server
+; NEEDS pip install cmake-language-server
+(add-hook 'cmake-mode-hook 'lsp-mode)
+
+; can you get usage hints as well?
